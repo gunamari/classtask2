9019ef163cc8523c97616fce9eeb0e1cfc26beea
gunamari <107184777+gunamari@users.noreply.github.com>
2022-10-15 00:04:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function foo(){
+    var res1=document.getElementById("Firstname").value;
+    var res2=document.getElementById("Middlename").value;
+    var res3=document.getElementById("Lastname").value;
+    var res4=document.getElementById("Email").value;
+    var res5=document.getElementById("Password").value;
+    console.log(res1,res2,res3,res4,res5);
+}
